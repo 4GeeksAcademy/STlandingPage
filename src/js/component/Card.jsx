118e57419db66ce5737a33447fb9445d860c3e5b
StevenSanz <163450220+StevenSanz@users.noreply.github.com>
2024-06-05 19:38:45 +0000

    STnew

---
+++ src/js/component/Card.jsx
@@ -0,0 +1,47 @@
+import React from "react"
+export const Card = () => {
+    return(
+        <div className="row">
+            <div className="col-sm-3">
+                <div className="card">
+                    <img src="https://cdn.pixabay.com/photo/2020/11/16/18/19/crocodile-5749911_640.jpg" className="card-img-top"/>
+                    <div className="card-body">
+                        <h5 className="card-title">A little Crocodile</h5>
+                        <p className="card-text">With a icecream, its the best. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error optio, laboriosam quos obcaecati excepturi eveniet in iusto nulla repudiandae assumenda nostrum reprehenderit temporibus culpa! Eos ex non nulla tempore iure.</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div className="col-sm-3">
+                <div className="card">
+                    <img src="https://cdn.pixabay.com/photo/2020/11/16/18/19/crocodile-5749911_640.jpg" className="card-img-top"/>
+                    <div className="card-body">
+                        <h5 className="card-title">A little Crocodile</h5>
+                        <p className="card-text">With a icecream, its the best. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis, modi, maiores aperiam cupiditate iusto nisi nobis aliquid, vitae doloribus saepe inventore itaque fuga sed et exercitationem laboriosam nam ducimus qui?</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div className="col-sm-3">
+                <div className="card">
+                    <img src="https://cdn.pixabay.com/photo/2020/11/16/18/19/crocodile-5749911_640.jpg" className="card-img-top"/>
+                    <div className="card-body">
+                        <h5 className="card-title">A little Crocodile</h5>
+                        <p className="card-text">With a icecream, its the best. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus optio accusantium omnis necessitatibus numquam fuga nostrum corporis ex voluptate perferendis? Eius veritatis hic eaque quam nemo perspiciatis eos rem dolore?</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div className="col-sm-3">
+                <div className="card">
+                    <img src="https://cdn.pixabay.com/photo/2020/11/16/18/19/crocodile-5749911_640.jpg" className="card-img-top"/>
+                    <div className="card-body">
+                        <h5 className="card-title">A little Crocodile</h5>
+                        <p className="card-text">With a icecream, its the best. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi esse blanditiis nemo aliquam debitis, dignissimos sequi incidunt? Molestiae fuga voluptatibus quae consequuntur ipsam itaque rem, dolores temporibus tempore nihil tempora.</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>   
+        </div>       
+    );
+    }
