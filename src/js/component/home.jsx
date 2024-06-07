3538e726598add56a00a3fcfb211c4cc970a0a98
StevenSanz <163450220+StevenSanz@users.noreply.github.com>
2024-06-07 17:27:27 +0000

    STfinal

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import { Navbar } from "./Navbar";
 import { Jumbotron } from "./Jumbotron";
 import { Card } from "./Card";
+import { Footer } from "./Footer";
 //create your first component
 const Home = () => {
 	return (
 		<div className="container">
 			<Navbar />
 			<Jumbotron />
+			<div className="row">
 			<Card />
+			<Card />
+			<Card />
+			<Card />
+			</div>
+			
+			<Footer />
 		</div>	
 	);
 };
